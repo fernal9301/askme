b3821950b21e4d2c0a939027a57a99c27d68407f
Sergey Ivanov <ivanovsergey1988@gmail.com>
2017-05-19 10:51:01 +0000

    add views and styles from lesson50-1 and support rails-i18n for ru lang

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'rails', '~> 5.0.2'
 gem 'sqlite3'
 gem 'puma', '~> 3.0'
 gem 'jquery-rails'
+gem 'rails-i18n'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,39 @@
 class UsersController < ApplicationController
-  def show
-    @hello = "Привет, Мишаня!"
+  def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/' \
+          '71269686e0f757ddb4f73614f43ae445?s=100'
+      ),
+      User.new(id: 2, name: 'Misha', username: 'aristofun'),
+      User.new(id: 3, name: '@Sergey', username: 'fernal9301')
+    ]
   end
+  
+  def new
+  end
+  
+  def edit
+  end
+  
+  def show
+    @user = User.new(
+      name: 'Sergey',
+      username: 'fernal9301',
+      avatar_url: nil
+    )
+  
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'Вопрос 3', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'Вопрос 4', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'Вопрос 5', created_at: Date.parse('27.03.2016'))
+    ]
+  
+    @new_question = Question.new
+    end
 end
