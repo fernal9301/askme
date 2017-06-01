bb117fa5feca18384c0ac3be3bed6649549e88f0
Sergey Ivanov <ivanovsergey1988@gmail.com>
2017-06-01 07:33:11 +0000

    add right localization to ru.yml and edit form user/edit

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ Bundler.require(*Rails.groups)
 
 module Askme
   class Application < Rails::Application
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
 
     config.time_zone = 'Moscow'
