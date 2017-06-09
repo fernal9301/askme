78b7ccb7662f5ce31c0076377466123f016e3bb0
fernal9301 <ivanovsergey1988@gmail.com>
2017-06-09 08:40:17 +0300

    add helper question_hashtags to view hashtags under question on hashtags/[:id]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,13 @@ module ApplicationHelper
     end
   end
 
+
+  def question_hashtags(question)
+    question.hashtags.map do |hashtag|
+      link_to("##{hashtag.name}", hashtag_path(hashtag))
+    end.join(' ').html_safe
+  end
+
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
