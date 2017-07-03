f1697d1f869801b3839cc5586a2c46dbda2da17d
Sergey Ivanov <ivanovsergey1988@gmail.com>
2017-07-03 06:29:15 +0000

    add dependent destroy

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,7 +1,7 @@
 class Hashtag < ApplicationRecord
   REGEXP = /#[[:alnum:]_]+/
 
-  has_many :hashtag_questions
+  has_many :hashtag_questions, dependent: :destroy
   has_many :questions, through: :hashtag_questions
 
   validates :name, uniqueness: true
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
-  has_many :hashtag_questions
+  has_many :hashtag_questions, dependent: :destroy
   has_many :hashtags, through: :hashtag_questions
   validates :text, :user, presence: true
   validates :text, length: { maximum: 255 }
